61e038ea1151e11287b4a7a2a1420f941a3c4614
sareeka61 <ghimiresarika61@gmail.com>
2024-08-01 00:15:53 +0545

    Add navigation

--- frontend/ecoquest/src/App.jsx
+++ frontend/ecoquest/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-import Navbar from './components/Navbar'
+import Navigation from './components/Navigation'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Navbar />
+      <Navigation />
     </>
   )
 }
