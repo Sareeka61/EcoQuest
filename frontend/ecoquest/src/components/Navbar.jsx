8094c261383c2fb558b75923d80313c751830924
sareeka61 <ghimiresarika61@gmail.com>
2024-08-01 11:02:26 +0545

    Complete navbar

--- frontend/ecoquest/src/components/Navbar.jsx
+++ frontend/ecoquest/src/components/Navbar.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
+import defaultProfile from '../assets/default-profile-image.png';
 
-const Navbar = () => {
+const HorizontalNavbar = () => {
+  const handleSignup = () => {
+    // Logic for login
+    console.log("Login clicked");
+  };
+
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="w-full bg-primary h-16 fixed top-0 left-0 flex items-center justify-between px-4 text-white shadow-lg">
+      <div className="flex items-center">
+        <img
+          src={defaultProfile}
+          alt="Logo"
+          className="w-8 h-8 rounded-full object-cover"
+        />
+        <span className="ml-2 text-2xl font-bold">Logo</span>
+      </div>
+      <div className="flex items-center">
+        <button
+          onClick={handleSignup}
+          className="bg-white hover:bg-slate-200 text-primary font-bold py-2 px-4 rounded mr-4 shadow-md hover:shadow-lg transition-shadow"
+        >
+          Signup
+        </button>
+        <img
+          src={defaultProfile}
+          alt="Profile"
+          className="w-8 h-8 rounded-full object-cover"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default HorizontalNavbar;
